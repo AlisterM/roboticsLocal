7f2d6b56de29d4dc1d7081bbfd1ab0b9fed29ba9
ThomasRansom <thomas.ransom@kcl.ac.uk>
2017-02-28 12:38:19 +0000

    almost done a*

--- Map.java
+++ Map.java
@@ -1,6 +1,9 @@
 package Mapping;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+
+import javax.swing.tree.TreeNode;
 
 public class Map {
 	boolean Grid[][];
@@ -14,7 +17,14 @@ public class Map {
 		
 		
 //--------------------------CODE TO FLL IN CORNERS----------------------
-		/*for(int q=0;q<19;q++){
+		for(int q=0;q<62;q++){
+			addLoc(0,q);
+			addLoc(61,q);
+			addLoc(q,0);
+			addLoc(q,61);
+		}
+		
+		for(int q=0;q<19;q++){
 		for(int r=0;r<=q;r++){
 				addLoc(44+q, 0+r);
 				
@@ -36,7 +46,7 @@ public class Map {
 			for(int r=0;r<=q;r++){
 				addLoc(44+q,62-r);
 			}//fills top right
-		}*/
+		}
 //----------------------------------END---------------------------------------
 	}
 	
@@ -60,11 +70,16 @@ public class Map {
 
 ////-----------------------------------------Test Stuff-----------------------------------
 public static void main(String[] args) {
-	Coordinate start = new Coordinate(1,2);
-	Coordinate end = new Coordinate(4,4);
-	Map test = new Map(5,5);
+	Coordinate start = new Coordinate(16,4);
+	Coordinate end = new Coordinate(50,50);
+	Map test = new Map(63,63);
 	Search tS = new Search(start, end, test);
-	System.out.println(tS.startSearch(start));
+	ArrayList<Coordinate> tesnode = tS.startSearch(start);
+	for(int i=0;i<tesnode.size();i++){
+		System.out.println(tesnode.get(i).x+" " + tesnode.get(i).y);
+		
+	}
+	
 	
 //	if(test.isFilled(44, 0)){
 //		System.out.println("filled");}
--- Search.java
+++ Search.java
@@ -1,10 +1,9 @@
+
 package Mapping;
 import javax.swing.tree.*;
 import java.util.ArrayList;
 import java.util.Iterator;
-import java.util.Stack;
-import java.util.TreeMap;
-//Should method return a stack or an arraylist?
+import java.util.ListIterator;
 class Search{
 	Coordinate startPoint;
 	Coordinate endPoint;
@@ -12,7 +11,9 @@ class Search{
 	DefaultTreeModel path;
 	ArrayList<Coordinate> openList;
 	ArrayList<Coordinate> closedList;
+	DefaultMutableTreeNode lastNode;
 	boolean endFound;
+	private ArrayList<Coordinate> nodePath;
 	//should search be an actual object
 	//What should be done in constructor vs in method?
 	Search(Coordinate sIn, Coordinate gIn, Map mIn){
@@ -33,13 +34,37 @@ class Search{
 	}
 	
 //----------------------------Methods--------------------------------------
-	public TreeNode[] startSearch(Coordinate sIn){
-		TreeNode[] nodePath = null;
+	//TODO Just fucking redo this whole mess
+	public ArrayList<Coordinate> startSearch(Coordinate sIn){
+		nodePath = new ArrayList<Coordinate>();
 		while(openList.size()>0&&endFound){
 			expand();
 		}
+		//Should get the last node, add to array and call getparent repaatedly until we arrive at the root of the tree.
 		if(!endFound){
-			nodePath=path.getPathToRoot(new DefaultMutableTreeNode(sIn));
+			DefaultMutableTreeNode currentNode = lastNode;
+			Coordinate test = (Coordinate) currentNode.getUserObject();
+			System.out.println(test.x);
+			System.out.println(test.y);
+			System.out.println(test.g);
+			System.out.println(test.h);
+			System.out.println(test.f);
+			nodePath.add(0, test);
+			
+			while(true){
+				//try{
+					currentNode = (DefaultMutableTreeNode) currentNode.getParent();
+					test=(Coordinate) currentNode.getUserObject();
+				//}catch(Exception e){
+				//	break;
+				//}
+				//finally{
+				//	break;
+				//}
+			
+			
+			}	
+			
 		}
 		
 		return nodePath;
@@ -75,7 +100,13 @@ class Search{
 		this.addOpen(c.x-1, c.y, c);
 		this.addOpen(c.x, c.y+1, c);
 		this.addOpen(c.x, c.y-1, c);
-		openList.remove(c);
+		ListIterator<Coordinate> openIt = openList.listIterator();
+		while(openIt.hasNext()){
+			Coordinate openCo = openIt.next();
+			if(openCo.x==c.x && openCo.y==c.y){
+				openIt.remove();
+			}
+		}
 		
 
 		//return null;
@@ -86,6 +117,13 @@ class Search{
 		//checks if space is occupied
 		if(!map.isFilled(xin, yin)){
 			//checks if the node is already in the open list
+			ListIterator<Coordinate> closedIt = closedList.listIterator();
+			while(closedIt.hasNext()){
+				Coordinate closedCo= closedIt.next();
+				if(closedCo.x==xin && closedCo.y==yin){
+					return;
+					}
+			}
 			if(openList.size()>0){
 				for(int i=0;i<openList.size();i++){
 					if(openList.get(i).x==xin&&openList.get(i).y==yin){
@@ -95,25 +133,26 @@ class Search{
 							openList.get(i).f=calcF(openList.get(i), parent.g);
 							break;
 						}
-						else{
-							//do nothing
-						}
-					} else{}
-					
-					//else{
-						//creates a new coordiante and adds it to the open list
-						int tempH = endPoint.manhatCompare(xin, yin, this.endPoint);
-						Coordinate temp = new Coordinate(xin, yin, (parent.g+1), tempH, (tempH+parent.g+1));
-						openList.add(temp);
-						path.insertNodeInto(new DefaultMutableTreeNode(temp), new DefaultMutableTreeNode(parent), 0);
-						System.out.println("node added 0");
-						//checks if the node that was just added is the endpoint. if it is the loop exits.
- 						if(temp.x==endPoint.x&&temp.y==endPoint.y){
-							endFound=false;
-						}
- 						break;
-					//}
+					}
 				}
+				
+					
+					
+				//creates a new coordiante and adds it to the open list
+				int tempH = endPoint.manhatCompare(xin, yin, this.endPoint);
+				Coordinate temp = new Coordinate(xin, yin, (parent.g+1), tempH, (tempH+parent.g+1));
+				DefaultMutableTreeNode tempNode = new DefaultMutableTreeNode(temp);
+				openList.add(temp);
+				path.insertNodeInto(tempNode, new DefaultMutableTreeNode(parent), 0);
+				System.out.println("node added 0");
+				//checks if the node that was just added is the endpoint. if it is the loop exits.
+				if(temp.x==endPoint.x&&temp.y==endPoint.y){
+					lastNode=tempNode;
+					endFound=false;
+				}
+				return;
+					
+				
 			}
 			else{
 				int tempH = endPoint.manhatCompare(xin, yin, this.endPoint);
@@ -125,8 +164,9 @@ class Search{
 				if(temp.x==endPoint.x&&temp.y==endPoint.y){
 					endFound=false;
 				}
-				
 			}
+				
+		
 	}
 	
 }
